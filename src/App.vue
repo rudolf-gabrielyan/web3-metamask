f74d55d9cf67d9f1efd9c77d66fd9fce685319d9
rudolf-gabrielyan <rudolf@tcf.team>
2023-09-04 16:12:38 +0400

    Web3 metamask integration | Graphql query

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Transactions Viewer Test Task</h1>
+        <wallet-button></wallet-button>
+        <hr />
+        <swap-list></swap-list>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WalletButton from './components/WalletButton.vue';
+import SwapList from './components/SwapList.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components: {
+        WalletButton,
+        SwapList,
+    },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
